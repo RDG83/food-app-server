6f2dfc0ef4bcf826cccb3769b4b9a615e8956c80
Robert de Grijs <rdgrijs@hotmail.com>
2020-05-01 14:53:06 +0200

    connect entries to db

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 var cors = require("cors");
 const app = express();
 const port = 4000;
 const bcrypt = require("bcrypt-nodejs");
-const knex = require('knex')
+const knex = require("knex");
 
 const db = knex({
-    client: 'pg',
+    client: "pg",
     connection: {
-        host: '127.0.0.1',
+        host: "127.0.0.1",
         user: process.env.DB_USER,
         password: process.env.DB_PW,
-        database: 'food_app'
-    }
+        database: "food_app",
+    },
 });
 
 app.use(express.json());
@@ -65,51 +65,56 @@ app.post("/signin", (req, res) => {
 // REGISTER ROUTE
 app.post("/register", (req, res) => {
     const { email, name, password } = req.body;
-    db('users').returning('*').insert({
-        name: name,
-        email: email,
-        joined: new Date(),
-    }).then(user => {
-        res.json(user[0])
-    }).catch(err => {
-        if (err.detail === "Key (email)=(april@gmail.com) already exists.") {
-            res.status(400).json('Unable to register, email already in use');
-        } else {
-            res.status(400).json(err.details);
-
-        }
-    })
+    db("users")
+        .returning("*")
+        .insert({
+            name: name,
+            email: email,
+            joined: new Date(),
+        })
+        .then((user) => {
+            res.json(user[0]);
+        })
+        .catch((err) => {
+            if (err.detail === "Key (email)=(april@gmail.com) already exists.") {
+                res.status(400).json("Unable to register, email already in use");
+            } else {
+                res.status(400).json(err.details);
+            }
+        });
 });
 
 // PROFILE ROUTE
 app.get("/profile/:id", (req, res) => {
     const { id } = req.params;
-    db.select('*').from('users').where({
-        id: id
-    }).then(user => {
-        if (user.length) {
-            res.json(user[0]);
-        } else {
-            res.status(400).json('Not found')
-        }
-    })
-        .catch(err => res.status(400).json('error getting user'))
+    db.select("*")
+        .from("users")
+        .where({
+            id: id,
+        })
+        .then((user) => {
+            if (user.length) {
+                res.json(user[0]);
+            } else {
+                res.status(400).json("Not found");
+            }
+        })
+        .catch((err) => res.status(400).json("error getting user"));
 });
 
 // IMAGE ROUTE
 app.put("/image", (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach((user) => {
-        if (user.id == id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    });
-    if (!found) {
-        res.json("failed to find user");
-    }
+    db.select("*")
+        .from("users")
+        .where("id", "=", id)
+        .increment("entries", 1)
+        .returning("entries")
+        .then((entries) => {
+            res.json(entries[0]);
+        }).catch(err => {
+            res.status(400).json('unable to find entries');
+        })
 });
 
 //   // Load hash from your password DB.
