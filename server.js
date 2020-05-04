95035827441dc281957ddd11d4dc6d2f682d99f2
Robert de Grijs <rdgrijs@hotmail.com>
2020-05-04 11:16:06 +0200

    refactor signin into controller

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const bcrypt = require("bcrypt-nodejs");
 const knex = require("knex");
 const salt = bcrypt.genSaltSync(10);
 const register = require("./controllers/register");
+const signin = require("./controllers/signin");
 
 const db = knex({
     client: "pg",
@@ -28,24 +29,7 @@ app.get("/", (req, res) => {
 });
 
 // SIGNING ROUTE
-app.post("/signin", (req, res) => {
-    db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(response => {
-            const isValid = bcrypt.compareSync(req.body.password, response[0].hash)
-            if (isValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('unable to get user'))
-            } else {
-                res.status(400).json('wrong credentials')
-            }
-        })
-        .catch(err => res.status(400).json('wrong credentials'))
-});
+app.post("/signin", (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 
 // REGISTER ROUTE
 app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt, salt) });
