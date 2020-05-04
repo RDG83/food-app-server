69788127691b7ca6b84aa26696f48914d6df0c61
Robert de Grijs <rdgrijs@hotmail.com>
2020-05-04 10:43:19 +0200

    add register controller

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = 4000;
 const bcrypt = require("bcrypt-nodejs");
 const knex = require("knex");
 const salt = bcrypt.genSaltSync(10);
+const register = require("./controllers/register");
 
 const db = knex({
     client: "pg",
@@ -47,35 +48,7 @@ app.post("/signin", (req, res) => {
 });
 
 // REGISTER ROUTE
-app.post("/register", (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password, salt);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx("users")
-                    .returning("*")
-                    .insert({
-                        name: name,
-                        email: loginEmail[0],
-                        joined: new Date(),
-                    })
-                    .then((user) => {
-                        res.json(user[0]);
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-    })
-        .catch((err) => {
-            res.status(400).json('unable to register')
-        });
-});
+app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt, salt) });
 
 
 
